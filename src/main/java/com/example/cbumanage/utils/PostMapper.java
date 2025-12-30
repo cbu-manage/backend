ff207988daed52a38bc9333d0edb9cd214ae7a45
김현민 <rlagusals1102@gmail.com>
2025-12-30 16:53:37 +0900

    feat: 스터디 모집 게시글 기능 초기 구현 - PostReport 코드를 기반으로 구현 - DELETE 기능 미구현 - GET 요청으로 스터디 게시글을 조회하려면 post, study 두개의 응답이 필요

--- src/main/java/com/example/cbumanage/utils/PostMapper.java
+++ src/main/java/com/example/cbumanage/utils/PostMapper.java
@@ -3,6 +3,7 @@ package com.example.cbumanage.utils;
 import com.example.cbumanage.dto.PostDTO;
 import com.example.cbumanage.model.Post;
 import com.example.cbumanage.model.PostReport;
+import com.example.cbumanage.model.PostStudy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,7 @@ public class PostMapper {
 
 
     public PostDTO.PostInfoDTO toPostInfoDTO(Post post) {
-        return  PostDTO.PostInfoDTO.builder().postId(post.getId())
+        return PostDTO.PostInfoDTO.builder().postId(post.getId())
                 .authorId(post.getAuthorId())
                 .title(post.getTitle())
                 .content(post.getContent())
@@ -27,28 +28,64 @@ public class PostMapper {
                 .build();
     }
 
+    public PostDTO.StudyInfoDTO toStudyInfoDTO(PostStudy study) {
+        return PostDTO.StudyInfoDTO.builder()
+                .status(study.getStatus())
+                .build();
+    }
+
+    public PostDTO.PostStudyCreateResponseDTO toPostStudyCreateResponseDTO(Post post, PostStudy study) {
+        return PostDTO.PostStudyCreateResponseDTO.builder()
+                .postId(post.getId())
+                .authorId(post.getAuthorId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .status(study.getStatus())
+                .createdAt(post.getCreatedAt())
+                .category(post.getCategory())
+                .build();
+    }
+
     /*
     아래는 각 Post{...}CreateRequestDTO 가 CreatePostDTO 를 만들 수 있게 하는 메소드 입니다
     매개변수의 DTO 를 바꾸면서 오버로딩하시면서 메소드 추가 하시면 됩니다
      */
-    public PostDTO.PostCreateDTO toPostCreateDTO(PostDTO.PostReportCreateRequestDTO req,Long userId) {
+    public PostDTO.PostCreateDTO toPostCreateDTO(PostDTO.PostReportCreateRequestDTO req, Long userId) {
         return PostDTO.PostCreateDTO.builder()
                 .authorId(userId)
                 .title(req.getTitle())
-                .content(req.getContent()).build();
+                .content(req.getContent())
+                .category(req.getCategory())
+                .build();
+    }
+
+    public PostDTO.PostCreateDTO toPostCreateDTO(PostDTO.PostStudyCreateRequestDTO req, Long userId) {
+        return PostDTO.PostCreateDTO.builder()
+                .authorId(userId)
+                .title(req.getTitle())
+                .content(req.getContent())
+                .category(req.getCategory())
+                .build();
     }
 
 
     /*
     아래는 게시물의 맞게 CreateDTO 를 반환해주는 메소드 입니다.Post 생성후에 postId를 매개변숯로 추가합니다
      */
-    public PostDTO.ReportCreateDTO toReportCreateDTO(PostDTO.PostReportCreateRequestDTO req,Long postId) {
+    public PostDTO.ReportCreateDTO toReportCreateDTO(PostDTO.PostReportCreateRequestDTO req, Long postId) {
         return PostDTO.ReportCreateDTO.builder().
                 postId(postId).
                 location(req.getLocation()).
                 startImage(req.getStartImage()).
                 endImage(req.getEndImage()).
                 date(req.getDate()).build();
+    }
+
+    public PostDTO.StudyCreateDTO toStudyCreateDTO(PostDTO.PostStudyCreateRequestDTO req, Long postId) {
+        return PostDTO.StudyCreateDTO.builder()
+                .postId(postId)
+                .status(req.getStatus())
+                .build();
     }
 
     /*
@@ -80,12 +117,24 @@ public class PostMapper {
                 .content(req.getContent()).build();
     }
 
+    public PostDTO.PostUpdateDTO toPostUpdateDTO(PostDTO.PostStudyUpdateRequestDTO req) {
+        return PostDTO.PostUpdateDTO.builder()
+                .title(req.getTitle())
+                .content(req.getContent()).build();
+    }
+
     public PostDTO.ReportUpdateDTO topostReportUpdateDTO(PostDTO.PostReportUpdateRequestDTO req) {
         return PostDTO.ReportUpdateDTO.builder()
                 .location(req.getLocation())
                 .StartImage(req.getStartImage())
                 .endImage(req.getEndImage())
                 .date(req.getDate())
+                .build();
+    }
+
+    public PostDTO.StudyUpdateDTO toStudyUpdateDTO(PostDTO.PostStudyUpdateRequestDTO req) {
+        return PostDTO.StudyUpdateDTO.builder()
+                .status(req.getStatus())
                 .build();
     }
 
