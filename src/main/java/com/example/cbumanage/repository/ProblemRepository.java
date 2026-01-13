b39f924f2dda338440b673c390a4cdee999331d0
junmin <coom1222@gmail.com>
2026-01-13 23:12:30 +0900

    Filter기능 구현 완료 - uri 매핑

--- src/main/java/com/example/cbumanage/repository/ProblemRepository.java
+++ src/main/java/com/example/cbumanage/repository/ProblemRepository.java
@@ -2,9 +2,11 @@ package com.example.cbumanage.repository;
 
 import com.example.cbumanage.model.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ProblemRepository extends JpaRepository<Problem, Integer> {
+public interface ProblemRepository extends JpaRepository<Problem, Integer>,
+        JpaSpecificationExecutor<Problem> {
 }
