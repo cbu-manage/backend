55cc1fd16642af57068808fb8502b4112ae23e72
dongan <dongan0618@naver.com>
2026-02-12 01:02:58 +0900

    feat(error):ERROR CODE 사용을 위한 customException 추가

--- src/main/java/com/example/cbumanage/exception/handler/RestControllerHandlerAdvice.java
+++ src/main/java/com/example/cbumanage/exception/handler/RestControllerHandlerAdvice.java
@@ -1,7 +1,10 @@
 package com.example.cbumanage.exception.handler;
 
 import com.example.cbumanage.authentication.exceptions.handler.ExceptionMessage;
+import com.example.cbumanage.exception.CustomException;
+import com.example.cbumanage.response.ResultResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,5 +22,10 @@ public class RestControllerHandlerAdvice {
 
 	public ExceptionMessage runtimeException(RuntimeException e, Integer status) {
 		return new ExceptionMessage(e.getClass().getName(), e.getMessage(), status);
+	}
+
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<ResultResponse<Void>> handleCustomException(CustomException e) {
+		return ResultResponse.error(e.getErrorCode());
 	}
 }
