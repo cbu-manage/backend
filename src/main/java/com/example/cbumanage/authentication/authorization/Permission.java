03f8ef32d3ae8a3791ca62e88bea1539e6839a9d
봉성은 <bongfam2@naver.com>
2026-02-18 15:39:57 +0900

    권한 수정

--- src/main/java/com/example/cbumanage/authentication/authorization/Permission.java
+++ src/main/java/com/example/cbumanage/authentication/authorization/Permission.java
@@ -28,7 +28,7 @@ public enum Permission {
 	 * - exclusivePath: 관리자 권한에서 제외할 경로
 	 */
 	ADMIN(
-			Set.of("/api/v1/member", "/api/v1/members"), // 관리자만 접근 가능한 경로
+			Set.of("/api/v1/member", "/api/v1/members", "/api/v1/*"), // 관리자만 접근 가능한 경로
 			Set.of()  // 관리자 권한에서 제외할 경로
 	);
 
