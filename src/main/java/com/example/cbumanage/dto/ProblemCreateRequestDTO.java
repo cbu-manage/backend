ddb8d98680e4d8ced67111b35dc3d1d80b213889
junmin <coom1222@gmail.com>
2026-03-01 23:29:15 +0900

    JSON 처리 위해 @Setter 추가

--- src/main/java/com/example/cbumanage/dto/ProblemCreateRequestDTO.java
+++ src/main/java/com/example/cbumanage/dto/ProblemCreateRequestDTO.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ import java.util.List;
  * 문제 생성할 때 사용하는 DTO.
  */
 @Getter
+@Setter
 @NoArgsConstructor
 public class ProblemCreateRequestDTO {
 
