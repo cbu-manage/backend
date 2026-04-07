dfc4cf9a8dcdb985e03c7dd459e6bc7e466a591e
김현민 <rlagusals1102@gmail.com>
2026-04-08 04:19:08 +0900

    feat: 미지정된 카테고리 추가

--- src/main/java/com/example/cbumanage/post/entity/enums/PostCategory.java
+++ src/main/java/com/example/cbumanage/post/entity/enums/PostCategory.java
@@ -1,7 +1,11 @@
 package com.example.cbumanage.post.entity.enums;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public enum PostCategory {
 
     @Schema(description = "스터디 모집 게시판")
@@ -10,16 +14,14 @@ public enum PostCategory {
     @Schema(description = "프로젝트 모집 게시판")
     PROJECT(2),
 
+    @Schema(description = "코딩테스트 문제 게시판")
+    PROBLEM(5),
+
+    @Schema(description = "자료실 게시판")
+    RESOURCE(6),
+
     @Schema(description = "보고서 업로드 게시판")
     REPORT(7);
 
     private final int value;
-
-    PostCategory(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
 }
