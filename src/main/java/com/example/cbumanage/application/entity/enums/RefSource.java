1a92e8445db0de55f5036e2a01cedd49243ff25d
JUNMIN <coom1222@gmail.com>
2026-07-04 01:07:15 +0900

    fix: 지원 경로를 기획에 맞게 수정
     EVERYTIME,  // 에브리타임
     INSTAGRAM,  // 인스타
     FRIEND,     //지인 추천
     ETC         // 기타, 별도 텍스트 필수

--- src/main/java/com/example/cbumanage/application/entity/enums/RefSource.java
+++ src/main/java/com/example/cbumanage/application/entity/enums/RefSource.java
@@ -4,9 +4,8 @@ package com.example.cbumanage.application.entity.enums;
  * 지원 경로입니다. ETC일때만 refSourceEtc로 텍스트를 받습니다.
  */
 public enum RefSource {
-    SNS,        // SNS
-    FRIEND,     // 지인 추천
-    POSTER,     // 포스터/현수막
-    WEBSITE,    // 홈페이지
+    EVERYTIME,  // 에브리타임
+    INSTAGRAM,  // 인스타
+    FRIEND,     //지인 추천
     ETC         // 기타, 별도 텍스트 필수
 }
